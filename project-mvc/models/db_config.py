ceb0f8746fbfee5b4ace41373694379d2e3a62cf
M Yerry Indra Setiawan <2313020254_yerry@mhs.univ.ac.id>
2026-06-23 16:41:43 +0700

    [SECURITY] Implement .gitignore and environment variables

---
+++ project-mvc/models/db_config.py
@@ -0,0 +1,11 @@
+# Konfigurasi koneksi database
+
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "rahasia_negara_123",
+    "database": "avolicius"
+}
+
+def get_db_config():
+    return DB_CONFIG
